c1d040577f7baac7406294239d294eacd81b7e39
IspahanRahman <ispahanrahman133@gmail.com>
2023-06-23 10:57:57 +0600

    footer is added

---
+++ app/component/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+const Footer = () => {
+  return (
+    <footer className="w-screen bg-white sticky top-[100vh] shadow dark:bg-gray-900 m-4">
+        <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+            <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">FeedAHelp</a>. All Rights Reserved.</span>
+        </div>
+    </footer>
+
+
+  )
+}
+
+export default Footer
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 import Image from 'next/image'
 import HomePage from '@/app/component/HomePage'
 import { getAllTodos } from '@/api'
+import Footer from './component/Footer';
 
 export default async function Home() {
   const todos = await getAllTodos();
   console.log(todos);
   return (
-    <main className='w-screen h-screen bg-gray-300 flex flex-row justify-center items-center'>
+    <main className='w-screen h-screen bg-gray-300 flex flex-col justify-center items-center'>
       <div className='w-3/4 overflow-y-auto h-2/3 bg-white border rounded-lg items-start'>
         <h1 className='text-4xl text-black font-bold mt-5 text-center'>TODO LIST</h1>
         <HomePage todos={todos}/>
       </div>
+      <Footer/>
     </main>
   )
 }
