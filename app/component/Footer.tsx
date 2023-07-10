426ed131e5316dd20b2cf4d4fa71c754435d68b8
IspahanRahman <ispahanrahman133@gmail.com>
2023-07-10 16:38:37 +0600

    navbar, footer changes

--- app/component/Footer.tsx
+++ app/component/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <footer className="w-screen bg-white sticky top-[100vh] shadow dark:bg-gray-900 m-4">
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-            <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">FeedAHelp</a>. All Rights Reserved.</span>
+            <span className="block text-sm text-gray-500 text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">FeedAHelp</a>. All Rights Reserved.</span>
         </div>
     </footer>
 
